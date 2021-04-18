1dd15b0503a133694fcd875196336c91df7e166c
sarahsbll <cynicoder@gmail.com>
2021-04-18 21:49:39 +0100

    title: Input and Output

---
+++ inoutput.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /* print the output to stdout */
+    int a, b, c;
+    /* get the input from stdin */
+    cin >> a >> b >> c;
+    cout << a + b + c << endl;
+
+    /* for n number of inputs: while (cin>n) sum+=n */
+
+    return 0;
+}
